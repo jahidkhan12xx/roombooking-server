3c2e36282defa1e5782117aa5a957f3c0dadecd2
Jahid Kham <jahidkhan12xx@gmail.com>
2023-11-05 14:23:00 +0600

    add filter in rooms

--- index.js
+++ index.js
@@ -5,13 +5,60 @@ require('dotenv').config()
 const port = process.env.PORT || 4000
 
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f3op28d.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const roomCollection = client.db("RoyalTaj").collection("Rooms")
+
+    app.get("/api/v1/rooms", async (req,res)=>{
+
+      let sortObj = {}
+
+      const sortField = req.query.sortField;
+      const sortOrder = req.query.sortOrder;
+
+      if(sortField && sortOrder){
+        sortObj[sortField] = sortOrder
+      }
+      const result = await roomCollection.find().sort(sortObj).toArray()
+      res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`This app is running on port ${port}`)
 })
